ca2070314040a02ab6649a5c5907646be496672a
Max Bronckers <maxbronckers@me.com>
2023-09-11 08:58:11 +0200

    adding lalt over backspace

--- keyboards/crkbd/keymaps/soundmonster_max/keymap.c
+++ keyboards/crkbd/keymaps/soundmonster_max/keymap.c
@@ -34,18 +34,11 @@ enum custom_keycodes {
   KC_RACL // right alt / colon
 };
 
-const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
-
-// This globally defines all key overrides to be used
-const key_override_t **key_overrides = (const key_override_t *[]){
-    &delete_key_override,
-    NULL // Null terminate the array of overrides!
-};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_split_3x6_3(
   //,-----------------------------------------.                ,---------------------------------------------.
-     KC_TAB,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                   KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_BSPC,
+     KC_TAB,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                   KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_LALT,
   //|------+------+------+------+------+------|                |------+------+-------+------+-------+--------|
     KC_LCTL,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                   KC_H,  KC_J,  KC_K,  KC_L, KC_SCLN,KC_QUOT,
   //|------+------+------+------+------+------|                |------+------+-------+------+-------+--------|
@@ -57,11 +50,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT_split_3x6_3(
   //,---------------------------------------------.                ,-----------------------------------------.
-     KC_ESC,  KC_NO, KC_7,   KC_8,  KC_9,   KC_NO,                    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+     KC_ESC, KC_MINS, KC_7,   KC_8,  KC_9, KC_ASTR,                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BSPC,
   //|------+------+-------+-------+-------+-------|                |------+------+------+------+------+------|
-    KC_LCTL, KC_NO, KC_4, KC_5,  KC_6,  KC_NO,                    KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,KC_NO,KC_NO,
+    KC_LCTL, KC_LALT, KC_4, KC_5,  KC_6,  KC_EQL,                   KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,KC_NO,KC_NO,
   //|------+------+-------+-------+-------+-------|                |------+------+------+------+------+------|
-    KC_LSFT, KC_0,  KC_1,  KC_2,  KC_3, KC_NO,                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_LSFT, KC_0,  KC_1,  KC_2,  KC_3, KC_PLUS,                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
   //|------+------+-------+-------+-------+-------+------|  |------+------+------+------+------+------+------|
                                    KC_LGUI, LOWER, KC_SPC,   KC_ENT, RAISE, KC_RALT
                                   //`--------------------'  `--------------------'
@@ -71,9 +64,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------.                ,-----------------------------------------.
      KC_ESC,KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,              KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_BSPC,
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    KC_LCTL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                KC_MINS,KC_EQL,KC_LCBR,KC_RCBR,KC_PIPE,KC_GRV,
+    KC_LALT, KC_NO, KC_NO, KC_LPRN,KC_RPRN, KC_EQL,             KC_MINS,KC_LCBR,KC_RCBR,KC_PIPE,KC_GRV, KC_DEL,
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    KC_LSFT, KC_F6, KC_F7, KC_F8, KC_F9,KC_F10,                KC_UNDS,KC_PLUS,KC_LBRC,KC_RBRC,KC_BSLS,KC_TILD,
+    KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_PLUS,               KC_UNDS,KC_LBRC,KC_RBRC,KC_BSLS,KC_TILD, KC_LCTL,
   //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
                                 KC_LGUI, LOWER, KC_SPC,   KC_ENT, RAISE, KC_RALT
                               //`--------------------'  `--------------------'
@@ -81,11 +74,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_ADJUST] = LAYOUT_split_3x6_3(
   //,-----------------------------------------.                ,-----------------------------------------.
-      QK_BOOT,RGBRST, KC_NO, KC_NO, KC_NO, KC_NO,                  KC_NO,KC_MUTE,  KC_NO, KC_NO, KC_NO, KC_NO,
+      QK_BOOT,RGBRST, KC_NO, KC_NO, KC_NO, KC_NO,                KC_NO, KC_F7, KC_F8, KC_F9, KC_F10, KC_NO,
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    RGB_TOG,RGB_HUI,RGB_SAI,RGB_VAI,RGB_SPI,KC_NO,               KC_PAUSE,KC_VOLU, KC_NO, KC_NO, KC_NO, KC_NO,
+    RGB_TOG,RGB_HUI,RGB_SAI,RGB_VAI,RGB_SPI,KC_NO,               KC_NO, KC_F4, KC_F5, KC_F6, KC_F11, KC_F12,
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    RGB_MOD,RGB_HUD,RGB_SAD,RGB_VAD,RGB_SPD,KC_NO,               KC_SCRL,KC_VOLD, KC_NO, KC_NO, KC_NO, RGB_RMOD,
+    RGB_MOD,RGB_HUD,RGB_SAD,RGB_VAD,RGB_SPD,KC_NO,               KC_NO,KC_F1, KC_F2, KC_F3, KC_NO, KC_NO,
   //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
                                 KC_LGUI, LOWER,KC_SPC,   KC_ENT, RAISE, KC_RALT
                               //`--------------------'  `--------------------'
@@ -325,17 +318,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           layer_off(_ADJUST);
         }
         return false;
-    // case KC_RACL:
-    //     if (record->event.pressed) {
-    //       my_colon_timer = timer_read();
-    //       register_code(KC_RALT);
-    //     } else {
-    //       unregister_code(KC_RALT);
-    //       if (timer_elapsed(my_colon_timer) < TAPPING_TERM) {
-    //         SEND_STRING(":"); // Change the character(s) to be sent on tap here
-    //       }
-    //     }
-    //     return false;
+    case KC_RACL:
+        if (record->event.pressed) {
+          my_colon_timer = timer_read();
+         register_code(KC_RALT);
+        } else {
+          unregister_code(KC_RALT);
+          if (timer_elapsed(my_colon_timer) < TAPPING_TERM) {
+            SEND_STRING(":"); // Change the character(s) to be sent on tap here
+          }
+        }
+        return false;
     case RGBRST:
       #ifdef RGBLIGHT_ENABLE
         if (record->event.pressed) {
